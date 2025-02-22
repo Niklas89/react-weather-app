5d4be52c9bb860b2839fbc7a967ae4ad5bbdd1be
niklas.edelstam <n.edelstam-ext@catalyse-it.com>
2025-02-22 23:14:53 +0100

    added docker files and env file

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '',
-		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	  'x-rapidapi-key': process.env.REACT_APP_GEO_API_KEY,
+	  'x-rapidapi-host': process.env.REACT_APP_GEO_API_HOST
 	}
-};
-
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
-
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "";
+  };
+  
+  export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL;
+  export const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL;
+  export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+  
